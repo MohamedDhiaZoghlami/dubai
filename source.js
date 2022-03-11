f4b86bdfee295f39f1ce97d2232da7e1cc4e6b5d
MohamedDhiaZoghlami <mohameddhia.zoghlami@esprit.tn>
2022-03-11 07:10:46 +0100

    form

--- source.js
+++ source.js
@@ -21,7 +21,6 @@ if (closeBtn) {
   });
 }
 let a = 0;
-
 dropBtn.addEventListener("click", () => {
   if (a == 0) {
     here.classList.add("shown");
@@ -29,5 +28,35 @@ dropBtn.addEventListener("click", () => {
   } else {
     here.classList.remove("shown");
     a = 0;
+  }
+});
+
+/*===========================================================*/
+let ticket = document.getElementById("ticket");
+let ticketPara = document.querySelector(".ticketPara");
+let ticketBonus = document.querySelector(".tickets");
+let pricePara = document.querySelector(".pricePara");
+let prix = document.querySelector(".price");
+let prixFinale = document.querySelector(".finalPrice");
+
+ticket.addEventListener("keyup", () => {
+  let promotion = 0;
+  let price = 30;
+  let totalPrice;
+  let finalPrice = price * (1 - promotion / 100);
+  if (ticket.value) {
+    ticketPara.style.display = "block";
+  }
+  let ticketNumber = Math.floor(ticket.value / 4);
+  ticketBonus.innerText = `${ticketNumber} tickets bonus`;
+  if (!promotion) {
+    pricePara.style.display = "block";
+    prix.innerText = finalPrice * ticket.value;
+  } else {
+    pricePara.style.display = "block";
+    prix.style.color = "#94514f";
+    prix.innerText = price * ticket.value;
+    prix.style.textDecoration = "line-through";
+    prixFinale.innerText = finalPrice * ticket.value;
   }
 });
